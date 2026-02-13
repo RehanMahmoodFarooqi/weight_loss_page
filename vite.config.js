f45cccc9e0e874acb67538a7453ab2521dce9bdd
Rehan Mahmood Farooqi <134191340+RehanMahmoodFarooqi@users.noreply.github.com>
2026-02-13 22:46:23 +0500

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/weight_loss_page/' : '/', // EXACT repo name for production
-}));
+})
+
