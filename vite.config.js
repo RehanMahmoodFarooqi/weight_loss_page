909de7024bb231a043f5300cb87141394a7c7ba6
Rehan Mahmood Farooqi <134191340+RehanMahmoodFarooqi@users.noreply.github.com>
2026-02-22 03:50:44 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: process.env.VERCEL ? '/' : (command === 'build' ? '/weight_loss_page/' : '/'),
-}));
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: env.VERCEL ? '/' : (command === 'build' ? '/weight_loss_page/' : '/'),
+  };
+});
 
